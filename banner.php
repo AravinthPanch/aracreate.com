3905088f393a17cf0c0b83b6e722aaeaf0bd4af1
Aravinth Panch <me@aravinth.info>
2020-03-29 00:28:47 +0100

    Update banner content

--- banner.php
+++ banner.php
@@ -6,16 +6,16 @@
           <div class="row align-items-center">
             <div class="col-lg-6 col-md-7">
               <div class="banner-content">
-                <h2 class="title" data-wow-duration=".65s" data-wow-delay=".1s">One Stop for Prototype to Production</h2>
-                <span class="sub-title" data-wow-duration=".65s" data-wow-delay=".3s">Engineering Service</span>
+                <h2 class="title" data-wow-duration=".65s" data-wow-delay=".1s">Prototype to Production</h2>
+                <span class="sub-title" data-wow-duration=".65s" data-wow-delay=".3s">Get your ideas researched, validated, prototyped, designed for manufacturing, certified and manufactured</span>
                 <div class="btn-wrapper">
-                      <a class="btn store-btn fill-style" data-wow-duration=".65s" data-wow-delay=".4s" href="https://aravinth.info/#portfolio"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="26px"><path fill-rule="evenodd"  fill="rgb(96, 56, 247)" d="M19.618,13.605 L15.283,16.531 L12.240,13.090 L15.570,9.324 L19.618,12.058 C19.857,12.219 20.000,12.508 20.000,12.832 C20.000,13.154 19.857,13.442 19.618,13.605 ZM0.662,-0.003 L10.427,5.850 L14.882,8.859 L11.691,12.469 L0.662,-0.003 ZM-0.000,25.689 L0.010,0.502 L11.142,13.090 L-0.000,25.689 ZM14.596,16.996 L10.443,19.799 L0.824,25.998 L11.691,13.710 L14.596,16.996 Z"/></svg> <span>Portfolio</span></a>
+                      <a class="btn store-btn fill-style" data-wow-duration=".65s" data-wow-delay=".4s" href="https://aravinth.info/#portfolio"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="26px"><path fill-rule="evenodd"  fill="rgb(96, 56, 247)" d="M19.618,13.605 L15.283,16.531 L12.240,13.090 L15.570,9.324 L19.618,12.058 C19.857,12.219 20.000,12.508 20.000,12.832 C20.000,13.154 19.857,13.442 19.618,13.605 ZM0.662,-0.003 L10.427,5.850 L14.882,8.859 L11.691,12.469 L0.662,-0.003 ZM-0.000,25.689 L0.010,0.502 L11.142,13.090 L-0.000,25.689 ZM14.596,16.996 L10.443,19.799 L0.824,25.998 L11.691,13.710 L14.596,16.996 Z"/></svg> <span>Engineering Service</span></a>
                     </div>
               </div>
             </div>
             <div class="col-lg-6 col-md-4 offset-lg-0 offset-md-1">
               <div class="img-wrapper img-right banner-img">
-                <img src="assets/images-this/aracreate-maker.png" alt="">
+                <img src="assets/images-this/aracreate-engineering.png" alt="">
               </div>
             </div>
           </div>
@@ -27,15 +27,15 @@
             <div class="col-lg-6 col-md-7">
               <div class="banner-content">
                 <h2 class="title" data-wow-duration=".65s" data-wow-delay=".1s">StartUp Mentorship</h2>
-                <span class="sub-title" data-wow-duration=".65s" data-wow-delay=".3s">Mentoring Service</span>
+                <span class="sub-title" data-wow-duration=".65s" data-wow-delay=".3s">Get connected to investors, accelerators, incubators, talents, manfacturers, research institutions, suppliers, etc</span>
                 <div class="btn-wrapper">
-                      <a class="btn store-btn fill-style" data-wow-duration=".65s" data-wow-delay=".4s" href="https://aravinth.info/#portfolio"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="26px"><path fill-rule="evenodd"  fill="rgb(96, 56, 247)" d="M19.618,13.605 L15.283,16.531 L12.240,13.090 L15.570,9.324 L19.618,12.058 C19.857,12.219 20.000,12.508 20.000,12.832 C20.000,13.154 19.857,13.442 19.618,13.605 ZM0.662,-0.003 L10.427,5.850 L14.882,8.859 L11.691,12.469 L0.662,-0.003 ZM-0.000,25.689 L0.010,0.502 L11.142,13.090 L-0.000,25.689 ZM14.596,16.996 L10.443,19.799 L0.824,25.998 L11.691,13.710 L14.596,16.996 Z"/></svg> <span>Portfolio</span></a>
+                      <a class="btn store-btn fill-style" data-wow-duration=".65s" data-wow-delay=".4s" href="https://aravinth.info/#portfolio"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="26px"><path fill-rule="evenodd"  fill="rgb(96, 56, 247)" d="M19.618,13.605 L15.283,16.531 L12.240,13.090 L15.570,9.324 L19.618,12.058 C19.857,12.219 20.000,12.508 20.000,12.832 C20.000,13.154 19.857,13.442 19.618,13.605 ZM0.662,-0.003 L10.427,5.850 L14.882,8.859 L11.691,12.469 L0.662,-0.003 ZM-0.000,25.689 L0.010,0.502 L11.142,13.090 L-0.000,25.689 ZM14.596,16.996 L10.443,19.799 L0.824,25.998 L11.691,13.710 L14.596,16.996 Z"/></svg> <span>Match Making Service</span></a>
                     </div>
               </div>
             </div>
             <div class="col-lg-6 col-md-4 offset-lg-0 offset-md-1">
               <div class="img-wrapper img-right banner-img">
-                <img src="assets/images-this/aracreate-maker.png" alt="">
+                <img src="assets/images-this/aracreate-match-make.png" alt="">
               </div>
             </div>
           </div>
